3f46edbcfc3480aef39adce0bbf453ec956f827c
Jitendra <jitendrapatwa135@gmail.com>
2018-09-02 12:51:56 +0530

    allowedit
    ok

--- www/js/app.js
+++ www/js/app.js
@@ -29,7 +29,7 @@ starter.controller("ExampleController", function($scope, $cordovaCamera) {
             quality : 75, 
             destinationType : Camera.DestinationType.DATA_URL, 
             sourceType : Camera.PictureSourceType.CAMERA, 
-            allowEdit : true,
+            allowEdit : false,
             encodingType: Camera.EncodingType.JPEG,
             targetWidth: 300,
             targetHeight: 300,
